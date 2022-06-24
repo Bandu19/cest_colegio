17c92757e5c00ab56d57eaad0be2f4ff9f75a709
Oscar Acuña <oscagustavo1961@gmail.com>
2022-06-24 18:49:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+   return (
+      <div class="container">
+         <div class="row">
+            <div class="col">Column</div>
+            <div class="col">Column</div>
+            <div class="col">Column</div>
+         </div>
+      </div>
+   );
+};
+
+export default App;
