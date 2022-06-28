f746a9b396f27e096b0ecdbc9740251563fab0e7
Oscar Acuña <oscagustavo1961@gmail.com>
2022-06-28 16:50:53 -0500

    Estructura NavBar-1

--- src/colegio/routes/CestMain.jsx
+++ src/colegio/routes/CestMain.jsx
@@ -4,7 +4,25 @@ import { Navbar } from "../../ui";
 export const CestMain = () => {
    return (
       <>
-         <Navbar />
+         <header className="header">
+            {/* CONTAINER PADRE */}
+            <div className="content_ani nav_mod ">
+               <Navbar />
+            </div>
+            <div className="container">
+               <div className="row">
+                  <div className="col-6">
+                     {/* Title */}
+                     <img
+                        className="imagen_estilo"
+                        src="../../../assets/SIEMPRE HAY UNA SEGUNDA.... FRASE.png"
+                        alt="imagen logo"
+                     />
+                     {/* Description */}
+                  </div>
+               </div>
+            </div>
+         </header>
       </>
    );
 };
--- src/ui/componentes/Navbar.jsx
+++ src/ui/componentes/Navbar.jsx
@@ -1,5 +1,35 @@
 // import { Link, NavLink } from "react-router-dom";
+import "../../css/index.css";
 
 export const Navbar = () => {
-   return <div>Navasr</div>;
+   return (
+      <div className="container">
+         <nav
+            className="
+               row text-white 
+               justify-content-between 
+               align-items-center text-uppercase "
+         >
+            {/* LOGO */}
+            <a href="#" className="col-auto">
+               <img
+                  src="../../../assets/CEST superior izquierdo.png"
+                  alt="Logo CEST"
+                  className="img-logo"
+               />
+            </a>
+
+            {/* ANCLAS */}
+
+            <div className="col-auto">
+               <a className="text-reset nav_items  items-cta " href="#">
+                  ¿Quienes Somos?
+               </a>
+               <a className="text-reset nav_items  items-cta" href="#">
+                  Iniciar Sesión
+               </a>
+            </div>
+         </nav>
+      </div>
+   );
 };
